b6b1cd07a0a049b7dfdc4c8b5eb4d89a33c4461d
ichorev <ichorev@users.noreply.github.com>
2024-05-19 09:56:55 +0700

    added script.lua for files

---
+++ script.lua
@@ -0,0 +1,10 @@
+/theme-park-game/
+|-- index.html
+|-- styles.css
+|-- script.js
+|-- /images/
+    |-- roller_coaster.png
+    |-- ferris_wheel.png
+    |-- carousel.png
+    |-- character_maleAdventurer_sheet.png
+    |-- background.jpg
